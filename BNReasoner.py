15fe983c24d08f9edbb87eb0b355432a433d3e23
gijssmeets <gijsolinsmeets@gmail.com>
2022-11-29 11:40:52 +0100

    started on pruning

--- BNReasoner.py
+++ BNReasoner.py
@@ -17,3 +17,46 @@ class BNReasoner:
 
     # TODO: This is where your methods should go
     print("hi")
+
+def run():
+    # bayesnet = BNReasoner("testing/lecture_example.BIFXML") 
+    bn = BayesNet()
+    bn.load_from_bifxml("testing/lecture_example.BIFXML")
+
+    # Obtain children
+    children = bn.get_children('Rain?')
+
+    for child in children:
+        # Obtain child node
+        cpt = bn.get_cpt(child)
+
+        # Delete edge
+        bn.del_edge(('Rain?',child))
+
+        # Get new CPT, given the evidence
+        new_cpt = bn.get_compatible_instantiations_table(pd.Series({'Rain?': False}),cpt)
+
+        # Delete the column with evidence
+
+        print(new_cpt)
+    
+
+
+    # print(bn.get_cpt(children[0]))
+    # print(bn.get_cpt(children[1]))
+
+
+
+
+
+
+    # bn.del_edge(self, edge):
+    #     """
+    #     Delete an edge form the structure of the BN.
+    #     :param edge: Edge to be deleted (e.g. ('A', 'B')).
+    #     """
+
+if __name__ == "__main__":
+    print("Start run ------------------------------------")
+    run()
+    print("End run --------------------------------------")
