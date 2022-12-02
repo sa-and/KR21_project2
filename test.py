73258f3497b276a299c70273a7b011e53c17efab
kimjongng88 <84227372+kimjongng88@users.noreply.github.com>
2022-12-02 11:40:36 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,13 @@ from BayesNet import BayesNet
 
 if __name__ == "__main__":
     print("begin")
-    net = "testing/dog_problem.BIFXML"
+    net = 'testing/lecture_example.BIFXML'
     bn = BayesNet()
     bn.load_from_bifxml(net)
+    #bn.draw_structure()
     bnr = BNReasoner(bn)
-    bn.draw_structure()
+    outcome = bnr.pruning({'Rain?': False,'Winter?':True})
+    #outcome.draw_structure()
+
+
+    #python3 test.py
