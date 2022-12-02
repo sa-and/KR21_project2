058c0b7b30abf1f97dbd28dcef936900b166fdf9
Bartvas <b.l.b.vanas@gmail.com>
2022-12-02 15:16:42 +0100

    Added Partial d_blocked + partial Marginalization

--- BNReasoner.py
+++ BNReasoner.py
@@ -16,10 +16,12 @@ class BNReasoner:
             self.bn = net
     # TODO: This is where your methods should go
 
+
+
+#Pruning
 def prune(net, q, e):
     node_prune(net, q, e)
     edge_prune(net, q, e)
-    BayesNet.draw_structure(net)
     return net
 
 def edge_prune(net, q, e): #TODO Update Factors see Bayes 3 slides page 28
@@ -29,7 +31,6 @@ def edge_prune(net, q, e): #TODO Update Factors see Bayes 3 slides page 28
             net.del_edge([node, edge])
     return net
 
-    
 def node_prune(net, q, e): #Performs Node Pruning given query q and evidence e
     for node in BayesNet.get_all_variables(net):
         if node not in q and node not in e:
@@ -37,13 +38,24 @@ def node_prune(net, q, e): #Performs Node Pruning given query q and evidence e
     return net
 
 
-#def d-blocked(x,y,z):
+def d_blocked(net, x,y,z):
+    #for w in x:   
+    return True 
+
+#def marginalization(net, variables):
+#    cpt = net.get_all_cpts()
+#    print(cpt)
+#    for variable in factor:
+
+    #totalp = sum(cpt["p"]) 
+
     
-#    for w in x:
-
-
+    #for variable in distribution:
+    #    if variable != target_node:
+    #       cpt = net.get_cpt(variable) 
+    #       totalp = sum(cpt['p'])
 
 
 
 net = BNReasoner("C:/Users/Bart/Documents/GitHub/KR21_project2/testing/dog_problem.BIFXML")
-prune(net.bn, [], ['bowel-problem'])
+#marginalization(net.bn, [], [])
