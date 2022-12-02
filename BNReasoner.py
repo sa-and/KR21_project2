e0afb1b378a69aa4da130c8add1964ecb0c0f8d8
Bartvas <b.l.b.vanas@gmail.com>
2022-12-02 14:42:31 +0100

    Added Pruning

--- BNReasoner.py
+++ BNReasoner.py
@@ -16,5 +16,34 @@ class BNReasoner:
             self.bn = net
     # TODO: This is where your methods should go
 
+def prune(net, q, e):
+    node_prune(net, q, e)
+    edge_prune(net, q, e)
+    BayesNet.draw_structure(net)
+    return net
+
+def edge_prune(net, q, e): #TODO Update Factors see Bayes 3 slides page 28
+    for node in e:
+        edges = net.get_children(node)
+        for edge in edges:
+            net.del_edge([node, edge])
+    return net
+
+    
+def node_prune(net, q, e): #Performs Node Pruning given query q and evidence e
+    for node in BayesNet.get_all_variables(net):
+        if node not in q and node not in e:
+            net.del_var(node)
+    return net
+
+
+#def d-blocked(x,y,z):
+    
+#    for w in x:
+
+
+
+
+
 net = BNReasoner("C:/Users/Bart/Documents/GitHub/KR21_project2/testing/dog_problem.BIFXML")
-BayesNet.draw_structure(net.bn)
+prune(net.bn, [], ['bowel-problem'])
