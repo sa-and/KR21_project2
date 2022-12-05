4d45f59bcb9bb809fec04aeea278a31b91737e2c
kimjongng88 <84227372+kimjongng88@users.noreply.github.com>
2022-12-05 09:32:56 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,17 @@ from BayesNet import BayesNet
 
 if __name__ == "__main__":
     print("begin")
-    net = 'testing/lecture_example.BIFXML'
+    net = 'testing/lecture_example.BIFXML' #'testing/dog_problem.BIFXML'
     bn = BayesNet()
     bn.load_from_bifxml(net)
     #bn.draw_structure()
     bnr = BNReasoner(bn)
     outcome = bnr.pruning(["Wet Grass?"],{'Rain?': False,'Winter?':True})
+    
+    ### D-seperation, use dog example
+    #outcome_d_not = bnr.d_separation(["family-out"], ["hear-bark"], ["light-on"]) # Not seperated
+    #outcome_d = bnr.d_separation(["family-out"], ["bowel-problem"], ["light-on"]) # Seperated  
+
     #outcome.draw_structure()
 
 
