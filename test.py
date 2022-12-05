b2020741226f9d4c28b685d699639f3fbfd8c47d
kimjongng88 <84227372+kimjongng88@users.noreply.github.com>
2022-12-05 18:15:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,26 @@ from BayesNet import BayesNet
 
 if __name__ == "__main__":
     print("begin")
-    net = 'testing/lecture_example.BIFXML' #'testing/dog_problem.BIFXML'
+    net = 'testing/dog_problem.BIFXML'
     bn = BayesNet()
     bn.load_from_bifxml(net)
-    #bn.draw_structure()
+    bn.draw_structure()
     bnr = BNReasoner(bn)
-    outcome = bnr.pruning(["Wet Grass?"],{'Rain?': False,'Winter?':True})
-    
-    ### D-seperation, use dog example
+    #outcome = bnr.pruning(["Wet Grass?"],{'Rain?': False,'Winter?':True})
+
+
+    ### D-seperation
     #outcome_d_not = bnr.d_separation(["family-out"], ["hear-bark"], ["light-on"]) # Not seperated
-    #outcome_d = bnr.d_separation(["family-out"], ["bowel-problem"], ["light-on"]) # Seperated  
+    #outcome_d = bnr.d_separation(["family-out"], ["bowel-problem"], ["light-on"]) # Seperated
+
+
+    ### Order min-degree
+    #outcome_min_degree = bnr.min_degree()
+    #print(outcome_min_degree)
+
+    ### Order min-degree
+    outcome_min_fill = bnr.min_fill()
+    print(outcome_min_fill)
 
     #outcome.draw_structure()
 
