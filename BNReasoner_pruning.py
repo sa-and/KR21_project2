fb19081c6f79781b4347f63a46e90b8e8d6874b0
Eva Boonstra <evaboonstra.eb@gmail.com>
2022-12-06 09:16:20 +0100

    I guess, dat beide orderings werken?

--- BNReasoner_pruning.py
+++ BNReasoner_pruning.py
@@ -1,6 +1,7 @@
 from typing import Union
 from BayesNet import BayesNet
 import pandas as pd
+from copy import deepcopy
 
 
 class BNReasoner_:
@@ -130,7 +131,7 @@ class BNReasoner_:
                     return False
                      
         return True 
-         
+
     def marginalization(self,bn,X):
         '''
         Given a factor and a variable X, compute the CPT in which X is summed-out
@@ -199,20 +200,138 @@ class BNReasoner_:
         # return h
         return h
 
+    def minimum_fill_ordering(self, bn):
+        '''
+        :param bn: Bayesnet of which an ordering needs to be returned based on minimum fill
+        :returns: a string containing an ordering based on minumum fill
+        '''
+        order = ""
+
+        #dictionary of all edges in the interaction graph
+        _, edges = self.least_edges(bn)
+
+        #saves number of added edges for each variable when that node would be removed
+        dict_added_edges = dict()
+        for var in BayesNet.get_all_variables(bn):
+            vars_edges = deepcopy(edges)
+            _, added_edges = self.connect_nodes(var, vars_edges)
+            dict_added_edges[var] = added_edges
+        #picks the node with the least number of added edges
+        var_to_del = sorted(dict_added_edges.items(), key=lambda item: item[1])[0][0]
+        order += var_to_del
+        
+        # remove node and return new edges 
+        edges, _ = self.connect_nodes(var_to_del, edges)
+
+        #for all remaining nodes pick the one that adds the least number of edges, add this to the ordering and 
+        # remove this from the node from the variables that still need to be added to the ordering, and 
+        # add edges if necessary 
+        for i in range(len(BayesNet.get_all_variables(bn)) - 1):
+            variables = list(edges.keys())
+            to_del_var = self.min_fill(variables, edges)
+            order += to_del_var
+            edges, _ = self.connect_nodes(to_del_var, edges)
+        return order
+
+    def min_fill(self, variables, edges):
+        '''
+        :param variables: a list with all variables that are not yet in the ordering
+        :param edges: a dictionary containing all the edges of the variables not yet in the ordering/ 
+                    stillin the interaction graph
+        :returns: the variable which would, when removed add the least number of edges
+        '''
+        dict_fill = dict()
+        for var in variables:
+            vars_edges = deepcopy(edges)
+            _, added_edges = self.connect_nodes(var, vars_edges)
+            dict_fill[var] = added_edges   
+        return sorted(dict_fill.items(), key=lambda item: item[1])[0][0]        
+        
+    def minimum_degree_ordering(self, bn):
+        '''
+        :param bn: Bayesian network for which an ordering needs to be returned based on minimum degree
+        :returns: a string containing an ordering based on minumum fill
+        '''
+        order = ""
+
+        #get the var that has the least edges and a dict containing for each variable the edges in the interaction graph
+        to_del_var, edges = self.least_edges(bn)
+        order += to_del_var
+
+        #remove var and add edges if necessary
+        edges, _ = self.connect_nodes(to_del_var, edges)
+        #while not all var are added to the ordering, pick var that has the least edges, 
+        # add this to the ordering and remove the var form the to added variables, add edges where necessary
+        for i in range(len(BayesNet.get_all_variables(bn)) - 1):
+            variables = list(edges.keys())
+            to_del_var = self.min_deg(variables, edges)
+            order += to_del_var
+            # print(to_del_var)
+            edges, _ = self.connect_nodes(to_del_var, edges)
+        return order
+
+    def min_deg(self, variables, edges):
+        '''
+        :param variables: a list with all variables that are not yet in the ordering
+        :param edges: a dictionary containing all the edges of the variables not yet in the ordering/ 
+                    still in the interaction graph
+        :returns: the variable with the least number of edges
+        '''
+        dict_degrees = dict()
+        for var in variables:
+            dict_degrees[var] = len(edges[var])  
+        # print(dict_degrees)      
+        return sorted(dict_degrees.items(), key=lambda item: item[1])[0][0]
+
+    def least_edges(self, bn):
+        All_var  = BayesNet.get_all_variables(bn)
+        dict_nr_edges = dict()
+        dict_edges = dict()
+        for var in All_var:
+            dict_nr_edges[var] = 0
+            dict_edges[var] = list()
+        
+        for var in All_var:
+            children = BayesNet.get_children(bn, var)
+            dict_nr_edges[var] += len(children)
+            for child in children:
+                dict_edges[var].append((var,child))                
+                dict_edges[child].append((child, var))
+                dict_nr_edges[child] += 1      
+        return sorted(dict_nr_edges.items(), key=lambda item: item[1])[0][0], dict_edges
+
+    def connect_nodes(self, var, edges):  
+        '''
+        adds edges between nodes if necessary when var is removed and removes var
+        and count the number of added edges
+        :param var: string containing the variable to be deleted
+        :param edges: dict containing all edges of each variable
+        :returns: a dict containing for all variables that are not yet in the ordering , the 
+                  in the graph, including the edges added when var is removed
+                : an integer that count the number of edges that are added when var is removed
+        '''
+        edge_added = 0            
+        for edge_1 in edges[var]:
+            edges[edge_1[1]].remove((edge_1[1], var))
+            for edge_2 in edges[var]:                              
+                if edge_1 != edge_2: 
+                    if (edge_2[1], edge_1[1]) not in edges[edge_2[1]]:                        
+                        edges[edge_2[1]].append((edge_2[1],edge_1[1]))
+                        edge_added += 1                    
+                    if (edge_1[1], edge_2[1]) not in edges[edge_1[1]]:                        
+                        edges[edge_1[1]].append((edge_1[1],edge_2[1]))                             
+        edges.pop(var)
+        return edges, edge_added
 
 
 
-
-
-
-
-     
 Pruning = False
 check_d_separation = False #True
 Independence = False #True
 Marginalization = False
 MaxingOut = False
 MultiplyFactor = False
+Ordering = True
 
 if Pruning:
     bnreasoner = BNReasoner_("testing/lecture_example.BIFXML")
@@ -265,6 +384,10 @@ if MultiplyFactor:
     cpt_2 = BayesNet.get_cpt(bnreasoner.bn,Y)
     bnreasoner.multiply_factors(cpt_1,cpt_2) 
 
+if Ordering:
+    bnreasoner = BNReasoner_("testing/lecture_example.BIFXML")
+    bnreasoner.minimum_degree_ordering(bnreasoner.bn)
+    bnreasoner.minimum_fill_ordering(bnreasoner.bn)
 
 
     
