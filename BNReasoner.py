40813ab190c44042a3c2ae93f72484870c119f22
ilsefeenstra <ilsef@me.com>
2022-12-07 10:25:44 +0100

    version two (not done)

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,5 +1,6 @@
 from typing import Union
 from BayesNet import BayesNet
+import pandas as pd
 
 
 class BNReasoner:
@@ -14,9 +15,30 @@ class BNReasoner:
             self.bn.load_from_bifxml(net)
         else:
             self.bn = net
+        
+        self.variables = self.bn.get_all_variables()
+        self.extended_factor = {}
 
-    def compute_factor(self):
-        pass
+    def print(self):
+        """
+        for testing purpose
+        """
+
+        all_ctps = self.bn.get_all_cpts()
+        
+        for ctp in all_ctps.keys():
+            test_cpt = all_ctps[ctp]
+        
+        #print(test_cpt)
+        new_cpt, extended_factor = self.maxing_out('Rain?', test_cpt)
+        print(new_cpt)
+
+        print(f'extended {extended_factor}')
+  
+
+           
+    
+        
     
     def marginalization(self, X, cpt):
         """
@@ -26,6 +48,8 @@ class BNReasoner:
         # Delete node X
         new_cpt = cpt.drop([X], axis=1)
         variables_left = [variable for variable in new_cpt.columns if variable != X and variable != 'p']
+
+        # Make case if there is only one variable
 
         # Take the sum of the factors
         new_cpt = new_cpt.groupby(variables_left).agg({'p': 'sum'})
@@ -37,26 +61,43 @@ class BNReasoner:
         """
         This function computes the CPT in which the variable X is maxed-out
         """
+
+        variables_left = [variable for variable in cpt.columns if variable != X and variable != 'p']
+        new_cpt = cpt.groupby(variables_left).agg({"p": "max"})
+        new_cpt.reset_index(inplace=True)
+
+        # Keep track of instatiation of X that led to maximized value
+        # change this 
+        extended_factor = cpt.groupby(X).agg({"p": "max"})
+        extended_factor = extended_factor.drop(["p"], axis=1)
+        extended_factor.reset_index(inplace=True)
+
+        #self.extended_factor[] = cpt.groupby.agg({"p": "max"})
         
-        # Delete node X
-        new_cpt = cpt.drop([X], axis=1)
-        variables_left = [variable for variable in new_cpt.columns if variable != X and variable != 'p']
 
-        # Take the max of the factors
-        new_cpt = new_cpt.groupby(variables_left).agg({'p': 'max'})
-        cpt.reset_index(inplace=True)
-
-        return new_cpt
+        return new_cpt, extended_factor
 
     def factor_multiplication(self, cpt1, cpt2):
         """
         This function computes the multiplied factor of two factors for two cpt's
         """
 
-        # Add an edge between every neighbour of 𝑋 that is not already connected by an edge
-        
-        pass
-        
+        cpt1_variables = list(cpt1.columns)
+        cpt2_variables = list(cpt2.columns)
+        common_variables = [variable for variable in cpt1_variables if variable in cpt2_variables and variable != 'p']
+
+        if not common_variables:
+            return 'ERROR: no common variables in CPTs, no multiplication possible'
+
+        cpt_combined = cpt1.merge(cpt2, left_on=common_variables ,right_on=common_variables, suffixes=('_1', '_2'))
+        cpt_combined['p'] = cpt_combined['p_1'] * cpt_combined['p_2']
+        cpt_combined = cpt_combined.drop(['p_1','p_2'], axis=1)
+
+        return cpt_combined
+
 if __name__ == "__main__":
     bayes = BNReasoner('testing/lecture_example.BIFXML')
+    bayes.print()
+    
+
     
