c85d3d981543184cf73658f706d7ab7d61f12983
Jeniamakarchik <eniamak@gmail.com>
2022-12-07 11:13:58 +0100

    add ordering and variable elimination

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,4 +1,5 @@
-from typing import Union
+from itertools import combinations, product
+from typing import Union, List
 from BayesNet import BayesNet
 
 
@@ -112,7 +113,144 @@ class BNReasoner:
         # Add an edge between every neighbour of 𝑋 that is not already connected by an edge
         
         pass
+
+    
+    def min_degree_ordering(self, X):
+        """
+        Given a set of variables X in the Bayesian network, 
+        compute a good ordering for the elimination of X based on the min-degree heuristics.
+        """
+
+        graph = self.bn.get_interaction_graph()
+
+        degrees = dict(graph.degree)
+        for node, _ in graph.degree:
+            if node not in X:
+                del degrees[node]
+        degrees = sorted(degrees.items(), key=lambda item: item[1])
+
+        order = []
+        while len(degrees):
+            node = degrees[0][0]
+            print(node)
+
+            # connect neighbours with each other
+            neighbours = list(graph.neighbors(node)) # get all the neighbours of the node, participating in order
+            for ind, _ in enumerate(neighbours):
+                neighbors_i = list(graph.neighbors(neighbours[ind])) # get all the neighbours from the current neibour
+                if ind < len(neighbours):
+                    for jnd in range(ind + 1, len(neighbours)): # check residual neighbours
+                        if neighbours[jnd] not in neighbors_i:
+                            graph.add_edge(neighbours[ind], neighbours[jnd])
+
+            # remove node from interaction graph
+            graph.remove_node(node)
+            order.append(node)
+
+            # recalculate degrees
+            degrees = dict(graph.degree)
+            for node, _ in graph.degree:
+                if node not in X:
+                    del degrees[node]
+            degrees = sorted(degrees.items(), key=lambda item: item[1])
+
+        return order
+
+    @staticmethod
+    def calculate_additional_edges(graph, node):
+        #TODO: rewrite with combinations
+        amount = 0
+        neighbours = list(graph.neighbors(node)) # get all the neighbours of the node
+        for ind, _ in enumerate(neighbours):
+            neighbors_i = list(graph.neighbors(neighbours[ind])) # get all the neighbours from the current neibour
+            if ind < len(neighbours):
+                for jnd in range(ind + 1, len(neighbours)): # check residual neighbours
+                    if neighbours[jnd] not in neighbors_i:
+                        amount += 1
         
+        return amount
+
+    def min_fill_ordering(self, X):
+        """Given a set of variables X in the Bayesian network, 
+        compute a good ordering for the elimination of X based on the min-fill heuristics.
+        """
+        graph = self.bn.get_interaction_graph()
+
+        amounts = dict()
+        for node in X:
+            amounts[node] = self.calculate_additional_edges(graph, node)
+        amounts = sorted(amounts.items(), key=lambda item: item[1])
+
+        order = []
+        while len(amounts):
+            node = amounts[0][0]
+            print(node)
+
+            # connect neighbours with each other
+            neighbours = list(graph.neighbors(node)) # get all the neighbours of the node, participating in order
+            for ind, _ in enumerate(neighbours):
+                neighbors_i = list(graph.neighbors(neighbours[ind])) # get all the neighbours from the current neibour
+                if ind < len(neighbours):
+                    for jnd in range(ind + 1, len(neighbours)): # check residual neighbours
+                        if neighbours[jnd] not in neighbors_i:
+                            graph.add_edge(neighbours[ind], neighbours[jnd])
+
+            # remove node from interaction graph
+            graph.remove_node(node)
+            order.append(node)
+
+            # recalculate degrees
+            amounts = dict()
+            for node in (X - set(order)):
+                amounts[node] = self.calculate_additional_edges(graph, node)
+            amounts = sorted(amounts.items(), key=lambda item: item[1])
+        
+        return order
+
+    def elimination_order(self, X, heuristic=None):
+        if heuristic is None:
+            order = X
+        elif heuristic == 'min_deg':
+            order = self.min_degree_ordering(X)
+        elif heuristic == 'min_fill':
+            order = self.min_fill_ordering(X)
+        else:
+            raise ValueError('Unknown ordering heuristic')
+
+        return order
+
+    def variable_elimination(self, cpt, X, heuristic=None):
+        """
+        Sum out a set of variables by using variable elimination. 
+        """
+        new_cpt = cpt.copy()
+        order = self.elimination_order(X, heuristic)
+
+        for node in order:
+            if node in new_cpt and len(new_cpt.columns) != 1:
+                new_cpt = self.marginalization(node, cpt)
+        
+        return new_cpt
+
+
+    def marginal_distributions(self, Q, e=None):
+        """
+        Given query variables Q and possibly empty evidence e, compute the marginal distribution P(Q|e). 
+        Note that Q is a subset of the variables 
+        in the Bayesian network X with Q ⊂ X but can also be Q = X. 
+        """
+
+        
+        
+        # reduce e
+        # compute probability Q and e - P(Q, e)
+        # compute probability of e
+        # compute P(Q, e)/P(e)
+
+        pass
+        
+
 if __name__ == "__main__":
     bayes = BNReasoner('testing/lecture_example.BIFXML')
+    bayes.min_degree_ordering({'I', 'J'})
     
