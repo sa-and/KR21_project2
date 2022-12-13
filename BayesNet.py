3044600942011b4bd096234679275add1e10130e
Nathan Jones <natmax.jones@gmail.com>
2022-12-13 18:18:49 +0100

    Assign random truth values to e in rand_Qe func

--- BayesNet.py
+++ BayesNet.py
@@ -306,11 +306,15 @@ class BayesNet:
         if q_first:
             Q = random.sample(nodes, num_Q)
             remaining_nodes = list(set(nodes) - set(Q))
-            e = random.sample(remaining_nodes, num_e)
+            e_vars = random.sample(remaining_nodes, num_e)
         else: # e first
-            e = random.sample(nodes, num_e)
+            e_vars = random.sample(nodes, num_e)
             remaining_nodes = list(set(nodes) - set(e))
             Q = random.sample(remaining_nodes, num_Q)
+
+        # Randomly assign truth values to variables in e
+        e = {node: random.choice([True, False]) for node in e_vars}
+        e = pd.Series(e)
         
         return (Q, e)
 
